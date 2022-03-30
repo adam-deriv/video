a47d6997908b9845918fd4da9a4d4d879bd27195
Adam Rybinski <adamrbnr@gmail.com>
2022-03-30 20:12:41 +0200

    Add signup

--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -4,25 +4,27 @@ import Input from "./components/Input";
 export default function Login() {
   return (
     <div className="mainWrapper">
-      <div className="topBar"><div className="topTitle">Sign In</div></div>
+      <div className="topBar"><div className="topTitle">Create an account</div></div>
       <div className="mainContent">
         <div className="formWrapper1">
           <Input title="Full name" placeholder="Enter your name" />
         </div>
-        <div className="formWrapper1">
+        <div className="formWrapper2">
           <Input title="Email address" placeholder="Enter your email" />
         </div>
         <div className="formWrapper2">
           <Input
             title="New Password"
             placeholder="Enter new password"
+            type="password"
+          />
         </div>
         <div className="formWrapper3">
-          <button className="buttonApp">Login</button>
+          <button className="buttonApp">Signup</button>
         </div>
         <div className="signupWrapper">
-          <div className="signupTitle">New here?&nbsp;</div>
-          <div className="interactiveText" onClick={() => appSerivce.send('GO_SIGNUP')}>Signup</div>
+          <div className="signupTitle">Already user?&nbsp;</div>
+          <div className="interactiveText" onClick={() => appSerivce.send('GO_LOGIN')}>Login</div>
         </div>
       </div>
     </div>
