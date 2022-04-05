cf1ebe7fb8bee6d0e92422cb2cf659fedd455c43
Adam Rybinski <adamrbnr@gmail.com>
2022-04-05 16:17:23 +0200

    Add plan tile

--- src/VideoAccount/VideoAccountPlan.jsx
+++ src/VideoAccount/VideoAccountPlan.jsx
@@ -1,3 +1,5 @@
+import './VideoAccountPlan.scss';
+
 export default function VideoAccountPlan() {
     return(
         <div className="mainWrapper">
@@ -6,6 +8,41 @@ export default function VideoAccountPlan() {
                     <div className="planTitle">
                         Free
                     </div>
+                    <div className="planDetail">
+                        <div className="planDetailIconYes" />
+                        <div className="planDetailSentence">
+                        &nbsp;Pellentesque interdum &nbsp;libero et
+                        </div>
+                    </div>
+                    <div className="planDetail">
+                        <div className="planDetailIconYes" />
+                        <div className="planDetailSentence">
+                        &nbsp;Pellentesque posuere &nbsp;jdfkdfkdfhd
+                        </div>
+                    </div>
+                    <div className="planDetail">
+                        <div className="planDetailIconYes" />
+                        <div className="planDetailSentence">
+                            Cras sed felis eget
+                        </div>
+                    </div>
+                    <div className="planDetail">
+                        <div className="planDetailIconYes" />
+                        <div className="planDetailSentence">
+                        Maecenas eget luctus
+                        </div>
+                    </div>
+                    <div className="planDetail">
+                        <div className="planDetailIconYes" />
+                        <div className="planDetailSentence">
+                        Nullam vitae augue
+                        </div>
+                    </div>
+                    <div className="planPrice" >
+                        <div className="planPriceCurrency">$</div>
+                        <div className="planPriceValue">0</div>
+                    </div>
+                    <div className="planButtonDowngrade"><button className="planButton">Downgrade</button></div>
                 </div>
             </div>
         </div>
