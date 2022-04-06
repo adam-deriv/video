95e28539b0fc09802a523c0a2b779956df3353b2
Adam Rybinski <adamrbnr@gmail.com>
2022-04-06 09:35:57 +0200

    Add init video create mobile

--- src/VideoCreate/VideoCreate.jsx
+++ src/VideoCreate/VideoCreate.jsx
@@ -1,39 +1,18 @@
 import { appService } from '../appService'
-import './VideoCreate.scss'
+import styles from'./VideoCreate.module.scss'
 
 export default function VideoCreate() {
     return (
         <div className="mainWrapper">
-            <div className="topBar">
-                <div className="topBarTitle">Saying Hi to my customers</div>
+            <div className={styles.topBar}>
+                <div className={styles.topBarTitle}>Saying Hi to my customers</div>
                 <div className="topBarRight">
-                    <button className="buttonAppSecondary" onClick={() => appService.send('GO_BROWSE')}>Save</button>
+                    <button className="buttonAppSecondary" onClick={() => appService.send('GO_BROWSE')}>Cancel</button>
                     <button className="buttonApp" onClick={() => appService.send('GO_BROWSE')}>Save</button>
                 </div>
             </div>
-            <div className="videoList">
-                <div className="videoItem">
-                    <div className="videoItemImage">
-                        <div className="videoItemOptions">
-                            <div className="videoItemOption">
-                                <button className="buttonApp">Edit</button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="videoItemTitle">
-                        Saying Hi to users!
-                    </div>
-                    <div className="videoItemTags">
-                        <div className="videoItemTag">
-                            Email
-                        </div>
-                        <div className="videoItemTag">
-                            Marketing
-                        </div>
-                        <div className="videoItemTag">
-                            Greeting
-                        </div>
-                    </div>
+`                <div className={styles.videoItem}>
+                <div className={styles.videoItemImage}>
                 </div>
             </div>
         </div>
